a06a0564e7ecc66e31a822fa34c5ea89309f1170
Thomas Maurin <thomas@maurin.io>
2016-12-19 14:16:30 +0100

    Update README

--- site/index.php
+++ site/index.php
@@ -1,4 +1,23 @@
 <!DOCTYPE html>
+
+<?php
+$table="attendees";
+$props = parse_ini_file("application.properties");
+$conn = new mysqli($props['dbhost'], $props['dbuser'], $props['dbpassword'], $props['database']);
+// check connection
+if ($conn->connect_error) {
+    echo 'Unable to connect to DB. Error: '  . $conn->connect_error;
+    exit();
+}
+$rs=$conn->query("select * from $table");
+if($rs === false) {
+    echo "Unable to retrieve data: ".$conn->error;
+} else {
+    $rs->data_seek(0);
+    $row = $rs->fetch_row();
+}
+?>
+
 <?php
 set_include_path(dirname(__FILE__) . '/../');
 
@@ -14,7 +33,7 @@ if (isset($props['endpoint'])) {
     $params['endpoint']=$props['endpoint'];
 }
 if (isset($props['key']) && isset($props['secret'])) {
-    $params['credentials'] = array( 
+    $params['credentials'] = array(
         'key' => $props['key'],
         'secret' => $props['secret']
     );
@@ -118,7 +137,7 @@ array_multisort($last,SORT_ASC, SORT_STRING,$first)
                 </div>
             </div>
         </div>
-    </header>   
+    </header>
 
     <!-- About Section -->
     <section class="success" id="about">
